3460fce3008beee85450a51456646a43e88b427e
mohit_kumar <152388769+mohitkumar64@users.noreply.github.com>
2026-03-05 10:39:56 +0530

    28-feb-2026

--- CircularPrime.java
+++ CircularPrime.java
@@ -12,6 +12,7 @@ class Main {
             System.out.println("number is prime");
             return true;
         }
+         if(n % 2 == 0) return false;
         for(int i = 3;i*i<=n;i++){
             if(n%i == 0){
                 
